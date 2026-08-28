94c5ee6b75e986919a2e37c3d2a34a18918931d5
l1716 <162274386+l1716@users.noreply.github.com>
2026-08-28 21:49:41 +0800

    style: unify bilingual typography and responsive layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router'
 
 function App() {
+  const { i18n } = useTranslation()
+  const documentLanguage = i18n.resolvedLanguage === 'zh-TW' ? 'zh-Hant' : 'en'
+
+  useEffect(() => {
+    document.documentElement.lang = documentLanguage
+  }, [documentLanguage])
+
   return (
     <BrowserRouter>
       <AppRouter />
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource-variable/roboto-condensed/wght.css'
+import '@fontsource-variable/roboto-condensed/wght-italic.css'
+import '@fontsource-variable/noto-sans-tc/wght.css'
 import App from './App.tsx'
 import './styles/globals.css'
 import './i18n'
